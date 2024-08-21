b7f6ee06ab7222ba29f15051be3362281b058235
Asif Jamal <107340041+asifjamalkmc@users.noreply.github.com>
2024-08-21 07:05:17 +0000

    variable and data types

---
+++ basic/datatypes.js
@@ -0,0 +1,14 @@
+'use strict' //treat all JS code as a newer version
+
+// alert(3 + 5);  we are using node.js not a browser
+
+
+// data types
+
+// number //let age = 20;
+// string //let name = "asif";
+// boolean //let isTrue = true; // true/false
+// Nan
+//null // let age = null; // null is a object
+//undefined // let age;
+//object
---
+++ basic/variable.js
@@ -0,0 +1,14 @@
+const accountId = "102Asif";
+let name = "Asif";
+var address = "Hyderabad";
+
+
+//this is also right to declare
+
+city = "Delhi";
+
+
+console.log(accountId);
+console.log(name);
+
+console.table([accountId, name, address, city]);
